23b8299abd99cb32565abbc15d0801808347a289
Nico Jayden Lehne <nicolehne@outlook.com>
2023-09-28 11:18:33 +0200

    Added function to read over text; Removed duplicate addition of spaces; Bump version (0.3.0)

--- calph/calph.c
+++ calph/calph.c
@@ -2,10 +2,11 @@
 #define fopen_s(pFile, filename, mode) ((*(pFile))=fopen((filename), (mode)))==NULL
 #endif
 
-#define CALPH_VERSION "0.2.0"
+#define CALPH_VERSION "0.3.0"
 
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define ERR_NO 0                // Successful: No Error
 #define ERR_GENERIC 1
@@ -26,39 +27,60 @@ size_t countArraySize = sizeof(count) / sizeof(*count);
               
 FILE* file;
 
-void counter() {  
-    for (int i = 0; i < (countArraySize); i++) {
-        for (int get = getc(file); get != EOF; get = getc(file))
-            if(get == characters[i] || get == toupper(characters[i]))
-                count[i]++;
-        fseek(file, 0, SEEK_SET);
+void help() {
+    printf("\nUse: calph [options] [input]\n\n");
+    printf("Available [options] are:\n\t\t\t--file\n\t\t\t\tTo open a file provided as filepath via [input].\n\t\t\t--text\n\t\t\t\tTo open text provided via [input]\n");
+}
+
+void counter(char * mode, char ** argv) {  
+    if (strcmp(mode, "file") == 0) {
+        for (int i = 0; i < (countArraySize); i++) {
+            for (int get = getc(file); get != EOF; get = getc(file))
+                if(get == characters[i] || get == toupper(characters[i]))
+                    count[i]++;
+            fseek(file, 0, SEEK_SET);
+        }
+    } else if (strcmp(mode, "text") == 0) {
+        for (int i = 0; i < (countArraySize); i++)
+            for (int k = 0; k < strlen(argv[2]); k++) {
+                char character = argv[2][k];
+                if(character == characters[i] || character == toupper(characters[i]))
+                    count[i]++;
+            }
     }
 }
 
 int main(int argc, char** argv) {
     if(!(argc > 1)) {
-        printf("Not enough arguments given, use calph --help to see syntax\nExiting\n");
+        printf("Not enough arguments given, use calph --help or calph -h to see syntax\nExiting\n");
         return ERR_NOARG;
-    }
-
-    int err = fopen_s(&file, argv[1], "r");
-    if (!(err == 0)) {
-        printf("File doesn't exist or is otherwise unavailable\nTry supplying a (valid) filename\n");
-        return ERR_NOFILE;
     }
 
     int sum;
 
-    counter();
+    if (strcmp(argv[1], "--file") == 0 || strcmp(argv[1], "-f") == 0) {
+        int err = fopen_s(&file, argv[2], "r");
+        if (!(err == 0)) {
+            printf("File doesn't exist or is otherwise unavailable\nTry supplying a (valid) filename\n");
+            return ERR_NOFILE;
+        }
+        counter("file", argv);
+    } else if (strcmp(argv[1], "--text") == 0 || strcmp(argv[1], "-t") == 0) {
+        counter("text", argv);
+    } else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        help();
+        return 0;
+    }
+
     for(int i = 0; i < countArraySize; i++) {
         sum+=count[i];
         if(!(count[i] == 0))
             printf("%c: %d\n", characters[i], count[i]);
     }
-    sum = sum + count[0];
     printf("Sum: %d characters\n", sum);
     printf("Sum (without spaces): %d characters\n", sum-count[0]);
 
-    fclose(file);
+    if (file != NULL)
+        fclose(file);
     return ERR_NO;
 }
